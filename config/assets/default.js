3c94290aa59bd4ae86d3740c7eeee8d1a0dac7ca
sangaArun <sanga2000@gmail.com>
2016-03-22 00:57:26 +0530

    admin-san first commit

--- config/assets/default.js
+++ config/assets/default.js
@@ -18,6 +18,7 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/dist/angular-file-upload.js',
+        'public/lib/owasp-password-strength-test/owasp-password-strength-test.js' ,
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js'
         // endbower
       ],
