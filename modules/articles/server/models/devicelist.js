816347df60abf7b46cdee1ecf4064d8324fb51c0
arunSanga <sanga2000@gmail.com>
2016-03-22 00:08:53 -0700

    node modules fast csv saving in database

---
+++ modules/articles/server/models/devicelist.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var deviceListSchema = new Schema({
+  ip: {
+    type: String,
+    default: ''
+  },
+  snmp_version: {
+    type: Number,
+    default: 0
+  },
+  snmpv3_user: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  snmpv3_auth: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  snmpv3_auth_key: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  snmpv3_privacy: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  snmpv3_privacy_key: {
+    type: String,
+    default: '',
+    trim: true
+  }
+});
+
+mongoose.model('DeviceList', deviceListSchema);
