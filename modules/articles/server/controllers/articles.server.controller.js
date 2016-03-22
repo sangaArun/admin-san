65e3d6b66bc681d9f64cdc4aa51efd59bbf8a469
arunSanga <sanga2000@gmail.com>
2016-03-22 00:23:20 -0700

    node modules fast csv saving in database

--- modules/articles/server/controllers/articles.server.controller.js
+++ modules/articles/server/controllers/articles.server.controller.js
@@ -18,7 +18,7 @@ var path = require('path'),
 // csvUpload = function (req, res) {
 //
 // };
-var file = path.join(__dirname, "device-list.csv");
+var file = path.join(__dirname, "device-list1.csv");
 var stream = fs.createReadStream(file);
 csv.fromStream(stream, { headers: ["ip", "snmp_version", "snmpv2_community", "snmpv3_user", "snmpv3_auth", "snmpv3_auth_key", "snmpv3_privacy", "snmpv3_privacy_key"] })
 .on("data", function(data) {
